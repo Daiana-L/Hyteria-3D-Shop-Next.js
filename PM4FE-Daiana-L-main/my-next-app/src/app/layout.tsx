6562e7005ab76a9f6d7beaa2c43dbbf8f80a2f65
Daiana-L <daiana_celeste-lopez@outlook.com>
2025-07-08 18:27:05 -0300

    overlay

--- PM4FE-Daiana-L-main/my-next-app/src/app/layout.tsx
+++ PM4FE-Daiana-L-main/my-next-app/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
             <head>
                 <link rel="icon" href="/assets/img/pet.png" />
             </head>
-            <body className="overflow-x-hidden">
+            <body>
                 <AOSInitializer />
                 {children}
             </body>
