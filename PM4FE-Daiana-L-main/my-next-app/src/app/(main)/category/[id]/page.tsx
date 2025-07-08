c4998e1df17bbcd543a06f0d845e39187368d983
Daiana-L <daiana_celeste-lopez@outlook.com>
2025-07-08 17:44:37 -0300

    manejo de errores

--- PM4FE-Daiana-L-main/my-next-app/src/app/(main)/category/[id]/page.tsx
+++ PM4FE-Daiana-L-main/my-next-app/src/app/(main)/category/[id]/page.tsx
@@ -9,7 +9,16 @@ type Props = {
 export default async function CategoryPage(props: Promise<Props>) {
     const { params } = await props;
     const categoryId = Number(params.id);
-    const products: Product[] = await fetchProductsByCategoryId(categoryId);
+
+    let products: Product[] = [];
+    let error = false;
+
+    try {
+        products = await fetchProductsByCategoryId(categoryId);
+    } catch (e) {
+        console.error("Error al obtener productos por categoría:", e);
+        error = true;
+    }
 
     const categoryName =
         products.length > 0
@@ -18,18 +27,27 @@ export default async function CategoryPage(props: Promise<Props>) {
 
     return (
         <div className="lg:px-4 lg:py-6">
-            <h2 className="lg:text-2xl xs:text-xl font-bold text-gray-900 6 xs:mt-6">
+            <h2 className="lg:text-2xl xs:text-xl font-bold text-gray-900 xs:mt-6">
                 Productos en la categoría / {categoryName}
             </h2>
 
             <div className="grid xs:grid-cols-2 lg:grid-cols-3 lg:gap-6 xs:gap-1">
-                {products.length === 0 && (
-                    <p>No hay productos en esta categoría.</p>
+                {error && (
+                    <p className="text-red-500 col-span-full">
+                        Hubo un error al conectar con el servidor. Intenta más tarde.
+                    </p>
                 )}
 
-                {products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
+                {!error && products.length === 0 && (
+                    <p className="col-span-full">
+                        No hay productos en esta categoría.
+                    </p>
+                )}
+
+                {!error &&
+                    products.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
             </div>
         </div>
     );
