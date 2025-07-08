d3b1fd4be19c851a85097021096e0792fa86ec04
Daiana-L <daiana_celeste-lopez@outlook.com>
2025-07-08 15:47:05 -0300

    .

--- PM4FE-Daiana-L-main/my-next-app/src/app/layout.tsx
+++ PM4FE-Daiana-L-main/my-next-app/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
             <head>
                 <link rel="icon" href="/assets/img/pet.png" />
             </head>
-            <body className="overflow-x-hidden">
+            <body className="overflow-x-hidden  overflow-y-hidden">
                 <AOSInitializer />
                 {children}
             </body>
