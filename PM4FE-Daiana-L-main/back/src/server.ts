4e746ecf24f9b87f1ce6518ed2d29a5b2cc909ec
Daiana-L <daiana_celeste-lopez@outlook.com>
2025-07-19 15:18:00 -0300

    methods

--- PM4FE-Daiana-L-main/back/src/server.ts
+++ PM4FE-Daiana-L-main/back/src/server.ts
@@ -22,8 +22,9 @@ const corsOptions: CorsOptions = {
     }
   },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
-
 
 
 app.use(cors(
