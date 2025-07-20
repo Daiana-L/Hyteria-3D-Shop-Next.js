09b88b8c8d3330439f5e539ab60e215ce00ae2be
Daiana-L <daiana_celeste-lopez@outlook.com>
2025-07-19 22:01:38 -0300

    .

--- PM4FE-Daiana-L-main/back/src/server.ts
+++ PM4FE-Daiana-L-main/back/src/server.ts
@@ -1,39 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
-import cors, { CorsOptions, CorsOptionsDelegate } from 'cors';
+import cors from 'cors';
 import router from "./routes";
 import morgan from "morgan";
-import { FRONTEND_URL, CRONJOB_URL } from "./config/envs";
+
 
 const app = express();
 
-
-const allowedOrigins: string[] = [FRONTEND_URL, CRONJOB_URL];
-
-const corsOptions: CorsOptions = {
-  origin: (origin, callback) => {
-    if (!origin) {
-      callback(null, true);
-      return;
-    }
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Origen no permitido por CORS"));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-
-app.use(cors(
-  corsOptions
-));
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.options('*', cors(corsOptions));
+
 app.use(router);
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
