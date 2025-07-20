2ba088283e3702930d69d342dea7c05e96d4ab2f
Daiana-L <daiana_celeste-lopez@outlook.com>
2025-07-19 22:21:03 -0300

    .

--- PM4FE-Daiana-L-main/back/src/server.ts
+++ PM4FE-Daiana-L-main/back/src/server.ts
@@ -6,7 +6,13 @@ import morgan from "morgan";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://hyteria-3d-shop.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(morgan("dev"));
 
